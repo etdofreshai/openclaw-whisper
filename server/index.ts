c8b042f92ab30a451a4762dcabe0542234418ced
ETdoFresh <etdofresh@gmail.com>
2026-02-19 02:14:30 +0000

    Fix static path: dist is at /app/dist not /app/server/dist

--- server/index.ts
+++ server/index.ts
@@ -321,7 +321,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const distPath = join(__dirname, '..', 'dist');
+const distPath = join(__dirname, '..', '..', 'dist');
 app.use(express.static(distPath));
 app.get('*', (_req, res) => { res.sendFile(join(distPath, 'index.html')); });
 
